7852fd4678a3b73b5c746691a0c829e53cd8e6b2
Test-web-android <tomchang.us@outlook.com>
2020-05-19 08:20:26 -0700

    Fixed Login Page

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -6,6 +6,7 @@ const LoginForm = () => {
       <input className="input-username" type="text" placeholder="Username" />
       <input className="input-password" type="text" placeholder="Password"/>
       <div className="forgot-password"><p>Forgot your password?&nbsp;<a href="/">Click here</a></p></div>
+      <div className="login-button">Login</div>
     </div>
   )
   
